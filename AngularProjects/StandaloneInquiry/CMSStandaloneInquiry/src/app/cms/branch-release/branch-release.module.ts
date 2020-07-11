bbce40db88d26c4bbf087fe9a06a307967188363
Edison Esguerra <edison.esguerra@consolsys.com>
2020-07-11 14:30:22 +0800

    adding CS library in the angular standalone

---
+++ AngularProjects/StandaloneInquiry/CMSStandaloneInquiry/src/app/cms/branch-release/branch-release.module.ts
@@ -0,0 +1,35 @@
+﻿import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CSMatFormsModule } from '@csnode/mat-forms';
+import { CommonModule } from '@angular/common';
+import { NgxMaskModule } from 'ngx-mask';
+import { RouterModule } from '@angular/router';
+import { BranchReleaseComponent } from './branch-release.component';
+import { CmsCommonModule } from '../cms-common.module';
+
+// import { TrantlrRptComponent } from './trantlr-rpt.component';
+
+@NgModule({
+  declarations: [
+    BranchReleaseComponent,
+  ],
+  imports: [
+	CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxMaskModule.forChild(),
+
+    CSMatFormsModule.forChild(),
+
+    RouterModule.forChild([{
+      path: '',
+      component: BranchReleaseComponent,
+    }]),
+
+    CmsCommonModule.forChild(),
+  ],
+  exports: [
+    BranchReleaseComponent,
+  ]
+})
+export class BranchReleaseModule { }
