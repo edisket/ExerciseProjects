1ee4ee2924041e7ed2d4b8181e16a3c15fe46673
Edison Esguerra <edisonesguerra.developer@gmail.com>
2020-08-08 01:56:05 +0800

    Angular training project

---
+++ AngularProjects/TaskApp/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormComponentComponent } from './form-component/form-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
